7edc4766313f317827806c52a02c72f93ec28b87
Oleg Miniuk <oleg.miniuk@gmail.com>
2018-03-15 10:10:15 +0300

    successfull request to yandex from api

--- middlewares/initRoutes.js
+++ middlewares/initRoutes.js
@@ -1,9 +1,27 @@
 const api = require('../routes/api/api');
 const path = require('path');
+const https = require('https');
 
 const initRoutes = app => {
-  app.use(`/test`, (req, res) => {
-    res.send('hello world');
+  app.use(`/`, (req, res) => {
+    const url = 'https://news.yandex.ru/politics.rss';
+    // const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    https.get(url, resp => {
+      let resultString = '';
+      resp.on('data', data => {
+        resultString += data;
+        // process.stdout.write(data);
+      });
+      resp.on('end', () => {
+        debugger;
+        // const result = JSON.parse(resultString);
+        const result = new DOMParser().parseFromString(resultString, 'text/xml');
+        debugger;
+      })
+    }).on('error', err => {
+      console.log(err);
+    }).end();
+    res.end();
   });
 
   app.use(`/api`, api);
