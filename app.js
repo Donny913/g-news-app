81772adb5560435aeaeb49bb8d2e31f3bd52c266
Oleg Miniuk <oleg.miniuk@gmail.com>
2018-03-17 23:08:50 +0300

    initStatic in app init

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require('express');
 
 const app = express();
 const {
-  initErrorHandler,
   initLogger,
   initParsers,
-  initRoutes,
   allowCORS,
+  initRoutes,
+  initStatic,
+  initErrorHandler,
 } = require('./middlewares/index');
 
 initLogger(app);
 initParsers(app);
 allowCORS(app);
 initRoutes(app);
+initStatic(app);
 initErrorHandler(app);
 
 module.exports = app;
--- middlewares/index.js
+++ middlewares/index.js
@@ -3,11 +3,13 @@ const initLogger = require('./initLogger');
 const initParsers = require('./initParsers');
 const initRoutes = require('./initRoutes');
 const allowCORS = require('./allowCORS');
+const initStatic = require('./initStatic');
 
 module.exports = {
-  initErrorHandler,
   initLogger,
   initParsers,
   initRoutes,
   allowCORS,
+  initStatic,
+  initErrorHandler,
 };
