36f76a7c70bac34f3242a6635f64f08d7a9a4fc3
Oleg Miniuk <oleg.miniuk@gmail.com>
2018-04-01 22:16:28 +0300

    version: 1.0.0

--- middlewares/initStatic.js
+++ middlewares/initStatic.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const initStatic = app => {
   app.use(express.static(path.join(__dirname, '../public')));
+  app.use((req, res) => {
+    res.redirect('/');
+  });
 };
 
 module.exports = initStatic;
